6e1c5792b766efd1376b47d68c4f7b0bd3f1b60d
VasuAgr2004 <130828602+VasuAgr2004@users.noreply.github.com>
2024-04-27 23:58:53 +0530

    Add files via upload

---
+++ STL-Intro.cpp
@@ -0,0 +1,24 @@
+/* 
+* must for competetive programming *
+STL - Standard Template Library 
+Library of generic classes and functions 
+Why use STL? - To use reusability : Well tested components, saves time.   
+Components of STL - (work together with co-ordination)
+    - Containers --> stores data 
+    - Algorithms --> set of procedures (sorting, searching) 
+    - Iterators  --> object points to an element in a container, handled just like pointers 
+                     and connects algorithms with containers 
+*/                    
+
+/* types of containers 
+Sequence containers --> stores data in linear fashion (Vectors, list, Dequeue)
+    1) Vector - Random Access faster, Insertion and deletion slower , insertion at end is faster  
+    2) List - Random Access slower, insertion in middle fast, Del/ins in end fast
+
+Associative containers --> direct access (Map/Multimap , Set/Multiset) - searching becomes faster and easier with trees 
+    - All operation are fast except random Access because it stores element in tree structure 
+ 
+Derived containers --> are derived sequence, asoociative containers and Real world modelling (Stack,Queue,Priority Queue)
+    - Depends on the data 
+*/
+
